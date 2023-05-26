2f79ee3b7281c2c97ec4502c71f630c542a68c65
qwerty0911 <81313471+qwerty0911@users.noreply.github.com>
2023-05-26 16:30:39 +0900

    first commit

---
+++ soLBTI/src/main/java/com/shinhan/soLBTI/SoLbtiApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.soLBTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoLbtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoLbtiApplication.class, args);
+	}
+
+}
---
+++ soLBTI/src/test/java/com/shinhan/soLBTI/SoLbtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.soLBTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoLbtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
