197075b328106fe1faa1286ac394473723ef6110
0seony <yseon406@gmail.com>
2023-05-31 16:09:13 +0900

    feat: header, footer 구조 생성

---
+++ front/solbti/src/common/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { MDBFooter } from "mdb-react-ui-kit";
+
+function Footer(props) {
+  return (
+    <MDBFooter bgColor="light" className="text-center text-lg-left">
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        &copy; {new Date().getFullYear()} Copyright:{" "}
+        <a className="text-dark" href="#">
+          solbti.site
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
+
+export default Footer;
