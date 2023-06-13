5e1effab0cbb8cb2b2d68b9109fc9b79e414cc44
qwerty0911 <81313471+qwerty0911@users.noreply.github.com>
2023-06-13 11:55:00 +0900

    feat mongodb connect

--- back/soLBTI/src/main/java/site/solbti/SoLbtiApplication.java
+++ back/soLBTI/src/main/java/site/solbti/SoLbtiApplication.java
@@ -2,12 +2,13 @@ package site.solbti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class SoLbtiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoLbtiApplication.class, args);
 	}
-
 }
--- back/soLBTI/src/main/java/site/solbti/repository/MembersRepository.java
+++ back/soLBTI/src/main/java/site/solbti/repository/MembersRepository.java
@@ -7,8 +7,7 @@ import java.util.Optional;
 
 public interface MembersRepository extends CrudRepository<Members, Long> {
     Optional<Members> findByMemId(String memId);
-
     Optional<Members> findByMemCode(Long id);
     Optional<Members> findByMemEmail(String email);
-
+    Optional<Members> findByMemPhone(String phone);
 }
