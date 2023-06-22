62dc217587570841f6f8dbaa3de91c52b70690bc
0seony <yseon406@gmail.com>
2023-06-22 17:51:30 +0900

    feat: 카드 추천 로직 구현

--- back/soLBTI/src/main/java/site/solbti/controller/QuizResultController.java
+++ back/soLBTI/src/main/java/site/solbti/controller/QuizResultController.java
@@ -3,15 +3,17 @@ package site.solbti.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import site.solbti.repository.CommonCardRepository;
 import site.solbti.repository.MembersRepository;
+import site.solbti.repository.MongoCommonCardRepository;
 import site.solbti.repository.PaymentHistoryRepository;
+import site.solbti.vo.CommonCard;
+import site.solbti.vo.MongoCommonCard;
 import site.solbti.vo.PersonalCard;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -20,6 +22,10 @@ public class QuizResultController {
     MembersRepository memRepo;
     @Autowired
     PaymentHistoryRepository historyRepo;
+    @Autowired
+    MongoCommonCardRepository mongoRepo;
+    @Autowired
+    CommonCardRepository commonRepo;
 
     @GetMapping("/result/{solbti}")
     public Map<Integer, String> solbtiResult(@PathVariable String solbti){
@@ -53,4 +59,28 @@ public class QuizResultController {
 
         return result;
     }
+
+    @GetMapping("/recommend")
+    public List<CommonCard> recommendCard(@RequestParam String category1, @RequestParam String category2, @RequestParam String category3) {
+        Map<String, Integer> category = new LinkedHashMap<>();
+        category.put(category1,10);
+        category.put(category2,5);
+        category.put(category3,3);
+        Map<Long, Integer> cardScore = new HashMap<>();
+        for(String key : category.keySet()) {
+            List<MongoCommonCard> cards = mongoRepo.findByBenefitCustum(key);
+            cards.stream().forEach(card->{
+                Long code = card.getCommonCardCode();
+                cardScore.put(code, cardScore.getOrDefault(code,0) + category.get(key));
+            });
+        }
+        List<Map.Entry<Long, Integer>> entryList = new LinkedList<>(cardScore.entrySet());
+        entryList.sort(((o1, o2) -> cardScore.get(o2.getKey()) - cardScore.get(o1.getKey())));
+        List<CommonCard> recommendCard = new ArrayList<>();
+        for(int i = 0; i<5; i++) {
+            recommendCard.add(commonRepo.findById(entryList.get(i).getKey()).orElse(null));
+        }
+        return recommendCard;
+    }
+
 }
