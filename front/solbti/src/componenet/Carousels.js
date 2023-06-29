295a0b5aab3df271dc43f605a0076a38886329bb
0seony <yseon406@gmail.com>
2023-06-29 16:31:01 +0900

    refactor(FE): 메인 페이지 슬라이드 사진 교체

--- front/solbti/src/componenet/Carousels.js
+++ front/solbti/src/componenet/Carousels.js
@@ -13,21 +13,21 @@ function Carousels(props) {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://api.card-gorilla.com:8080/storage/display/3713/pc_img/29121/%EC%B9%B4%EB%93%9C%EA%B3%A0%EB%A6%B4%EB%9D%BC_%EB%A9%94%EC%9D%B8%EC%85%80%EB%A0%89%EC%85%98_PC.jpg"
+          src="/img/carousels/banner1.png"
           alt="First slide"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://api.card-gorilla.com:8080/storage/display/4281/pc_img/28919/230420_16380_KB%EA%B5%AD%EB%AF%BC%EC%B9%B4%EB%93%9C_%EC%B9%B4%EB%93%9C%EA%B3%A0%EB%A6%B4%EB%9D%BC_%EB%A9%94%EC%9D%B8-%EC%85%80%EB%A0%89%EC%85%98PC_860X340_v4.jpg"
+          src="/img/carousels/banner2.png"
           alt="Second slide"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://api.card-gorilla.com:8080/storage/display/4292/pc_img/29191/44_CardGorilla_MainSelection%28%EB%A9%94%EC%9D%B8%EC%85%80%EB%A0%89%EC%85%98%29_PC_860x340.jpg"
+          src="/img/carousels/banner3.png"
           alt="Third slide"
         />
       </Carousel.Item>
