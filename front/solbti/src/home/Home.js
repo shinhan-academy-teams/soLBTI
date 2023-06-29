815535b22f2b532ff206f2bf49531e72c3c84bbc
0seony <yseon406@gmail.com>
2023-06-29 15:30:06 +0900

    refactor(FE): 가로 스크롤 삭제

--- front/solbti/src/home/Home.js
+++ front/solbti/src/home/Home.js
@@ -1,5 +1,4 @@
 import Carousels from "componenet/Carousels";
-import HorizontalCardList from "componenet/HorizontalCardList";
 import React from "react";
 import { Container } from "react-bootstrap";
 
@@ -7,7 +6,6 @@ function Home(props) {
   return (
     <Container>
       <Carousels />
-      <HorizontalCardList />
     </Container>
   );
 }
